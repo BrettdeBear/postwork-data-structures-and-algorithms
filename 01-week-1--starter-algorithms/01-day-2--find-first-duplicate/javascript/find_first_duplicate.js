2e9aefd412a11888b0d7e4b78a14b9f536174337
Brett de Bear <bkdebear@gmail.com>
2023-06-02 09:37:59 -0700

    included pseudocode for explaining function

--- 01-week-1--starter-algorithms/01-day-2--find-first-duplicate/javascript/find_first_duplicate.js
+++ 01-week-1--starter-algorithms/01-day-2--find-first-duplicate/javascript/find_first_duplicate.js
@@ -25,3 +25,10 @@ module.exports = findFirstDuplicate;
 
 // Please add your pseudocode to this file
 // And a written explanation of your solution
+
+// in line 1, I set up a function that takes in an array
+// In line 2, we are introducing a constant variable that equals an empty array, and this is where we are storing the frequency of each number
+// Next, I set up a for loop that starts at 0, continues as long as i is less than the length of the array, and increments the count each time
+// If the duplicates array includes the number, then we return that number.
+// If not, it adds the number to the end of the duplicates array
+// If there are no duplicates, the function returns -1
