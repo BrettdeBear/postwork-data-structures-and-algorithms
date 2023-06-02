90eb322003e3f8398c3a22159b4f487144ccd14a
Brett de Bear <bkdebear@gmail.com>
2023-06-02 09:43:39 -0700

    added pseudocode to explain function

--- 01-week-1--starter-algorithms/02-day-3--fibonacci-series/javascript/fibonacci.js
+++ 01-week-1--starter-algorithms/02-day-3--fibonacci-series/javascript/fibonacci.js
@@ -31,3 +31,11 @@ module.exports = fibonacci;
 
 // Please add your pseudocode to this file
 // And a written explanation of your solution
+
+// In line 1, I declare a function that takes in a specific number that represents the number of fibonacci numbers to generate
+// Next, I set up a constant variable equal to an empty array. This is used to store all the fib numbers
+
+// In line 4, there is a for loop that begins at 0, continues as long as i is less than or equal to the number passed into the function, and increments each time through
+// In line 5, I set up an if statement. If the integer is less than 2, those numbers need to be added to the array so the series can begin
+// If it is not <= 2, then we add to the array the sum of the previous two numbers 
+// In line 11, we return the output of the number that was passed into the function.
