9fc201795ce32636b95b12e3beaf375c353d0b80
Brett de Bear <bkdebear@gmail.com>
2023-06-21 10:46:55 -0700

    for loop solution and pseudocoded explanation

--- 01-week-1--starter-algorithms/04-day-5--find-shortest-string/javascript/find_shortest_string.js
+++ 01-week-1--starter-algorithms/04-day-5--find-shortest-string/javascript/find_shortest_string.js
@@ -1,5 +1,12 @@
 function findShortestString(arr) {
-  // type your code here
+  let shortestString = arr[0];
+
+  arr.forEach(element => {
+    if (element.length < shortestString.length) {
+      shortestString = element;
+    }
+  })
+  return shortestString
 }
 
 if (require.main === module) {
@@ -18,9 +25,33 @@ if (require.main === module) {
   console.log("=>", findShortestString(['flower', 'juniper', 'lily', 'dadelion']));
 
   // BENCHMARK HERE
+  // const startTime = Date.now();
+
+  // for (let i = 0; i < 1000; ++i) {
+  //   findShortestString(['dogbaby', 'cat', 'jammy', 'hamtaro']);
+  // }
+
+  // const avgTime = (Date.now() - startTime) / 1000;
+  // console.log(avgTime);
+
 }
 
 module.exports = findShortestString;
 
 // Please add your pseudocode to this file
 // And a written explanation of your solution
+
+// Brainstorming:
+
+// Step 1--estab var that has index of element?
+// Step 2--iterate over each element, and if it's shorter than the one in the variable, we make that the variable. 
+
+// Explanation: 
+
+// Line 1: Define function that takes in an array
+// Line 2: establish variable that is index 0 of the array (originally did const, but that wasn't the move because it couldn't change)
+// Line 4: iterate through each element of the array
+// Line 5: If the length of that element is shorter than the shortestString variable, 
+// Line 6: change the shortestString to that element (that element takes its place)
+// Line 9: shortestString is returned
+
