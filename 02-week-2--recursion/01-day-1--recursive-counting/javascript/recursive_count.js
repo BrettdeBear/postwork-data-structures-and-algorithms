0ca3a572147e054591f7bdd7119d9934b9aeed78
Brett de Bear <bkdebear@gmail.com>
2023-06-22 13:23:23 -0700

    recursive solution -- explanation still needed

--- 02-week-2--recursion/01-day-1--recursive-counting/javascript/recursive_count.js
+++ 02-week-2--recursion/01-day-1--recursive-counting/javascript/recursive_count.js
@@ -1,5 +1,9 @@
 function recursiveCount(num = 0) {
-  // type your code here
+  if (num >= 10) {
+    return undefined;
+  }
+  console.log(num)
+  recursiveCount(num + 1);
 }
 
 if (require.main === module) {
